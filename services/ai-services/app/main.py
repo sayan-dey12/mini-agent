458d0a92d51ebf13e7bf4509ea14066ad2825dd0
SAYAN <sayandey1203@gmail.com>
2026-06-29 23:57:51 +0530

    schemas in python and modified main for chat

--- services/ai-services/app/main.py
+++ services/ai-services/app/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
 
+from app.schemas.chat import ChatRequest
+from app.schemas.chat import ChatResponse
+
 app = FastAPI()
+
 
 @app.get("/")
 async def home():
-    return {"message": "Mini Agent AI Service Running 🚀"}
+    return {
+        "message": "Mini Agent AI Service Running 🚀"
+    }
+
+
+@app.post("/chat", response_model=ChatResponse)
+async def chat(request: ChatRequest):
+
+    return ChatResponse(
+        text=f"Python received: {request.message}"
+    )

09592fbfbac41e8700fd75be8f49cbe5104cb19c
SAYAN <sayandey1203@gmail.com>
2026-06-29 13:00:05 +0530

    improve testcommand

--- apps/cli/src/commands/TestCommand.ts
+++ apps/cli/src/commands/TestCommand.ts
@@ -1,13 +1,15 @@
 import { Agent } from "@mini-agent/agents";
+import type { AgentRequest } from "@mini-agent/agents";
 
+const message: AgentRequest = {
+    message: "Hello Agent!",
+}
 
 export class TestCommand {
   async execute(): Promise<void> {
     const agent = new Agent();
 
-    const response = await agent.execute({
-      message: "Hello Agent!",
-    });
+    const response = await agent.execute(message);
 
     console.log(response.text);
   }

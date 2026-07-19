b9934a2cae95e88eaf5321b2879cc79168cff3e8
SAYAN <sayandey1203@gmail.com>
2026-07-19 21:20:12 +0530

    implements those in Chat command

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,3 +2,6 @@ export * from "./ApplicationContainer.js";
 
 export * from "./config/config.js";
 
+export * from "./config/interfaces/IConfigService.js";
+
+export * from "./config/services/FileConfigServices.js";
--- packages/shared/src/config/AgentConfig.ts
+++ packages/shared/src/config/AgentConfig.ts
@@ -1,6 +1,6 @@
 export interface AgentConfig{
     provider: string;
     model: string;
-    mode: 'stream' | 'full';
+    mode: 'stream' | 'full' | 'exit';
     temperature: number;
 }

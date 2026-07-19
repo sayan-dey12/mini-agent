7428fb0229b5c4f81713085ab77318cc772485c9
SAYAN <sayandey1203@gmail.com>
2026-07-20 03:05:18 +0530

    modify Agent Request type

--- packages/agents/src/types/AgentRequest.ts
+++ packages/agents/src/types/AgentRequest.ts
@@ -2,4 +2,8 @@ import { ChatMessage } from "@mini-agent/shared";
 
 export interface AgentRequest {
     messages: ChatMessage[];
+     config?: {
+        model: string;
+        temperature: number;
+    };
 }
